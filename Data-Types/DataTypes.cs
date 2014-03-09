301546e351f36a239de328809e9a38f347c1e4bd
themarys <maria.stoycheva@gmail.com>
2014-03-09 17:04:41 +0200

    Yes, pull it.
    Please upload these.

---
+++ Data-Types/DataTypes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class DataTypes
+{
+    static void Main()
+    {
+        ushort first = 52130;
+        sbyte second = -115;
+        int third = 4825932;
+        byte forth = 97;
+        short fifth = -10000;
+
+        Console.WriteLine(second);
+        Console.WriteLine("The five numbers are: {0}, {1}, {2}, {3} and {4}.", first, second, third, forth, fifth);
+
+        if (second < 0)
+        {
+            Console.WriteLine("I am no fool");
+        }
+
+    }
+}
+
